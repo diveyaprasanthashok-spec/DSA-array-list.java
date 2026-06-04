83ac7753131f123efc59e24d34691eb91892b278
diveyaprasanthashok-spec <diveyaprasanth.ashok@gmail.com>
2026-06-04 11:18:01 +0530

    Add files via upload

---
+++ Arraylistdemo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraylistdemo;
+
+/**
+ *
+ * @author hp
+ */
+import java.util.ArrayList;
+public class Arraylistdemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ArrayList<String> fruits = new ArrayList<>();
+       fruits.add("apple");
+       fruits.add("banana");
+       fruits.add("mango");
+       System.out.println(fruits);
+    }
+    
+}
